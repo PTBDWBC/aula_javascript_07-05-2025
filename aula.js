064022de2a6a667b0f8c68cf1716e0232fbd8d23
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-05-07 19:56:55 -0300

    Create aula.js

---
+++ aula.js
@@ -0,0 +1,13 @@
+/*
+Javascript
+
+- interpretado
+- Para páginas dinâmicas
+- possui variáeis, funções e eventos
+- pode ser inserido dentro (não recomendado) ou fora do html (arquivo)
+- hierarquia de objetos
+- <script> no html
+- document: métodos para manipular objetos
+- innerHTML é um atributo do parágrafo
+- 
+*/
