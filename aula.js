acd2668b31f7d5e0a3cf7f2577337898df4a8075
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-05-07 20:13:53 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -6,8 +6,11 @@ Javascript
 - possui variáeis, funções e eventos
 - pode ser inserido dentro (não recomendado) ou fora do html (arquivo)
 - hierarquia de objetos
-- <script> no html
+- o que estiver em <script> no html, será o que deve estar no arquivo javascript
+- atributos do <script>: src, async (ativa execução assincrona), charset (caracteres), defer (contrario do async, script executa quando a página finaliza o carregamento)
+- em <script>, utilizar src="nomedoarquivo.js" para conectar o javascript ao html
 - document: métodos para manipular objetos
 - innerHTML é um atributo do parágrafo
+- onclick -> com o clique, pode direcionar para uma função
 - 
 */
