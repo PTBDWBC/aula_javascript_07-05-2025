15cd5175997fcdd368ab29b3f223ef74a3064918
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-05-07 21:56:31 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -13,5 +13,13 @@ Javascript
 - innerHTML é um atributo do parágrafo
 - onclick -> com o clique, pode direcionar para uma função
 - alert segura o carregamento da página
-- 
+- let: determina que a variável pertence aquele bloco (local)
+- var: pode ser acessada fora do bloco ou dentro, depende de onde for criada
+- const: não muda
+- arrays: podem ter elementos de diversos tipos dentro do mesmo array
+- arrays: criar: (var nome = [];), inserir valor: (nome[0] = "valor"; ou var nome = ["a", "b", "234567890"];)
+- sintaxe json: .,  
+- json: objetos
+- funções: globais ou locais
+- funções: 
 */
